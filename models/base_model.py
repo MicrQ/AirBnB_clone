3a0361690d4ae0ea62a7282a662ff96b2846fcb4
MicrQ <mail.micrq@proton.me>
2024-02-10 18:25:02 +0300

    BaseModel class modified to take args

--- models/base_model.py
+++ models/base_model.py
@@ -8,8 +8,16 @@ from datetime import datetime
 class BaseModel:
     """represents all classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializer"""
+        if len(kwargs) > 0:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k == "created_at" or k == "updated_at":
+                        v = datetime.fromisoformat(v)
+                    setattr(self, k, v)
+            return
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
