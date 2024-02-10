2fb1d6502586871b1b47156a4ae3c9ee84308c79
MicrQ <mail.micrq@proton.me>
2024-02-10 12:57:42 +0300

    modified

--- models/base_models.py
+++ models/base_models.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-"""  """
+""" Base Class for all classes """
+
+import uuid
+""" used to generate unique id """
+
+
+class BaseModel:
+    """represents all classes"""
+
+    def __init__(self):
+        """Initializer"""
+        self.id = str(uuid.uuid4())
