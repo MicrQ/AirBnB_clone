e13ac348c3093eb51ca2e7cc9d6fa50f8444fc02
MicrQ <mail.micrq@proton.me>
2024-02-10 13:07:24 +0300

    modified

--- models/base_models.py
+++ models/base_models.py
@@ -2,6 +2,7 @@
 """ Base Class for all classes """
 
 import uuid
+import datetime
 """ used to generate unique id """
 
 
@@ -11,3 +12,4 @@ class BaseModel:
     def __init__(self):
         """Initializer"""
         self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime
