5fdc61cfe81773df303e74f1521a2eb555fb2c02
MicrQ <mail.micrq@proton.me>
2024-02-11 21:12:24 +0300

    Console modified: create, show, destroy, update and all commands done

--- console.py
+++ console.py
@@ -4,8 +4,10 @@
 import cmd
 from models.base_model import BaseModel
 from models import storage
+import re
+import json
 
-clas_s = {'BaseModle': BaseModel}
+clas_s = {'BaseModel': BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -36,18 +38,17 @@ class HBNBCommand(cmd.Cmd):
         if not valid(args):
             return
         obj = clas_s[args[0]]()
+        obj.save()
         print(obj.id)
 
     def do_show(self, arg):
         """Displays string representation of instance"""
         args = arg.split()
-        if not valid(args):
+        if not valid(args, id=True):
             return
-        if len(args) < 2:
-            print("** instance id missing **")
-            return
+
         objects = storage.all()
-        key = "{}.{}".format(args[0],args[1])
+        key = "{}.{}".format(args[0], args[1])
         str_repr = objects.get(key)
 
         if str_repr is None:
@@ -55,8 +56,80 @@ class HBNBCommand(cmd.Cmd):
             return
         print(str_repr)
 
+    def do_destroy(self, arg):
+        """Deletes an instance based on class name and id"""
+        args = arg.split()
+        if not valid(args, id=True):
+            return
 
-def valid(self, args):
+        objects = storage.all()
+        key = "{}.{}".format(args[0], args[1])
+        str_repr = objects.get(key)
+
+        if str_repr is None:
+            print('** no instance found **')
+            return
+        del objects[key]
+        storage.save()
+
+    def do_all(self, arg):
+        """Prints a string representation of instances"""
+        args = arg.split()
+        objects = storage.all()
+
+        if len(args) < 1:
+            print(["{}".format(str(v)) for _, v in objects.items()])
+            return
+        if args[0] not in clas_s.keys():
+            print("** class doesn't exist **")
+            return
+        else:
+            print(["{}".format(str(v))
+                  for _, v in objects.items() if type(v).__name__ == args[0]])
+            return
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        args = arg.split()
+        if not valid(args, id=True):
+            return
+        objects = storage.all()
+        key = "{}.{}".format(args[0], args[1])
+        str_repr = objects.get(key)
+
+        if str_repr is None:
+            print('** no instance found **')
+            return
+
+        # cmp_json = re.findall(r"{.*}", arg)
+        # if cmp_json:
+        #     pld = None
+        #     try:
+        #         pld: dict = json.loads(cmp_json[0])
+        #     except Exception:
+        #         print("** invalid syntax")
+        #         return
+        #     for k, v in pld.items():
+        #         setattr(objects, k, v)
+        #     storage.save()
+        #     return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+        if len(args) < 4:
+            print("** value missing **")
+            return
+        attr = re.findall(r"^[\"\'](.*?)[\"\']", args[3])
+        if attr:
+            setattr(str_repr, args[2], attr[0])
+        else:
+            values = args[3].split()
+            setattr(str_repr, args[2], parseStr(values[0]))
+        storage.save()
+
+
+def valid(args, id=False):
     """validates given argument for command"""
     if len(args) < 1:
         print("** class name missing **")
@@ -64,7 +137,45 @@ def valid(self, args):
     if args[0] not in clas_s:
         print("** class doesn't exist **")
         return False
+    if len(args) < 2 and id:
+        print("** instance id missing **")
+        return False
     return True
+
+
+def is_float(x):
+    """Checks if x is float."""
+    try:
+        a = float(x)
+    except (TypeError, ValueError):
+        return False
+    else:
+        return True
+
+
+def is_int(x):
+    """Checks if x is int.
+    """
+    try:
+        a = float(x)
+        b = int(a)
+    except (TypeError, ValueError):
+        return False
+    else:
+        return a == b
+
+
+def parseStr(arg):
+    """Parse to int || float || str
+    """
+    parsed = re.sub("\"", "", arg)
+
+    if is_int(parsed):
+        return int(parsed)
+    elif is_float(parsed):
+        return float(parsed)
+    else:
+        return arg
 
 
 if __name__ == '__main__':
