2f3f92e4ae6b7dbbf3bc651b8cfb9573e5ef6978
MicrQ <mail.micrq@proton.me>
2024-02-11 11:13:02 +0300

    emptyline and some helps modified

--- console.py
+++ console.py
@@ -9,13 +9,21 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
     def do_exit(self, arg):
-        """to exit the interpreter"""
+        """to exit the interpreter, type: exit"""
         return True
     
     def do_EOF(self, arg):
         """to exit interpreter when EOF detected"""
         print("")
         return True
+    
+    def do_help(self, arg):
+        """To get help, type: help <topic>"""
+        return super().do_help(arg)
+
+    def emptyline(self):
+        """ to execute nothing when emptyline entered"""
+        pass
     
 
 if __name__ == '__main__':
