21e0e97b23dc75031926f2e9f6720289a15bb5c9
MicrQ <mail.micrq@proton.me>
2024-02-11 12:33:18 +0300

    create command and show command handled

--- console.py
+++ console.py
@@ -2,6 +2,10 @@
 """ The command line Interpreter"""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
+
+clas_s = {'BaseModle': BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -25,6 +29,42 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ to execute nothing when emptyline entered"""
         pass
+
+    def do_create(self, arg):
+        """Creates a new Instance"""
+        args = arg.split()
+        if not valid(args):
+            return
+        obj = clas_s[args[0]]()
+        print(obj.id)
+
+    def do_show(self, arg):
+        """Displays string representation of instance"""
+        args = arg.split()
+        if not valid(args):
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        objects = storage.all()
+        key = "{}.{}".format(args[0],args[1])
+        str_repr = objects.get(key)
+
+        if str_repr is None:
+            print('** no instance found **')
+            return
+        print(str_repr)
+
+
+def valid(self, args):
+    """validates given argument for command"""
+    if len(args) < 1:
+        print("** class name missing **")
+        return False
+    if args[0] not in clas_s:
+        print("** class doesn't exist **")
+        return False
+    return True
 
 
 if __name__ == '__main__':
