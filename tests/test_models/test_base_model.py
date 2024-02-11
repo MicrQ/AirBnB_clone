abebb6db5a91f491c2902d790188aab829e72b23
MicrQ <mail.micrq@proton.me>
2024-02-11 10:04:24 +0300

    test for basemodel added

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,6 +17,28 @@ class test_BaseModel(unittest.TestCase):
         self.assertIsInstance(BaseModel().created_at, datetime)
         self.assertIsInstance(BaseModel().updated_at, datetime)
 
+    def test_save_no_args(self):
+        """Tests save() with no arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.save()
+        msg = "save() missing 1 required positional argument: 'self'"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_save_excess_args(self):
+        """Tests save() with too many arguments."""
+        self.resetStorage()
+        with self.assertRaises(TypeError) as e:
+            BaseModel.save(self, 98)
+        msg = "save() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(e.exception), msg)
+
+    def test_str(self):
+        """Test method for str representation"""
+        b1 = BaseModel()
+        string = f"[{type(b1).__name__}] ({b1.id}) {b1.__dict__}"
+        self.assertEqual(b1.__str__(), string)
+
     def test_checkMethods(self):
         """checking their method"""
         test = BaseModel()
@@ -25,6 +47,7 @@ class test_BaseModel(unittest.TestCase):
         self.assertNotEqual(test.updated_at, a)
 
         self.assertIsInstance(BaseModel().to_dict(), dict)
+
 
 if __name__ == "__main__":
     unittest.main()
