22758ee30234144ec35fd0fbd263c2d449f3ca68
MicrQ <mail.micrq@proton.me>
2024-02-11 11:14:47 +0300

    pycodestyle

--- console.py
+++ console.py
@@ -8,15 +8,16 @@ class HBNBCommand(cmd.Cmd):
     """ Represents The Interprater """
 
     prompt = "(hbnb) "
+
     def do_exit(self, arg):
         """to exit the interpreter, type: exit"""
         return True
-    
+
     def do_EOF(self, arg):
         """to exit interpreter when EOF detected"""
         print("")
         return True
-    
+
     def do_help(self, arg):
         """To get help, type: help <topic>"""
         return super().do_help(arg)
@@ -24,7 +25,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ to execute nothing when emptyline entered"""
         pass
-    
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
